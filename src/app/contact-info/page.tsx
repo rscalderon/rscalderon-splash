18ec48b8e0adf42f32f3dfead4093c92d419ce09
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-12-28 10:26:41 -0600

    contact info page

---
+++ src/app/contact-info/page.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
+        <h1 className='text-xl'>Contact me</h1>
+        <ol className='list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
+          <li className='flex justify-between space-x-6'>
+            <p>First Name:</p> <p>Rodrigo</p>
+          </li>
+          <li className='flex justify-between space-x-6'>
+            <p>Last Names:</p> <p>Samour Calderon</p>
+          </li>
+          <li className='flex justify-between space-x-6'>
+            <p>Phone:</p> <p>+1-510-990-5400</p>
+          </li>
+          <li className='flex justify-between space-x-6'>
+            <p>Email:</p> <p>samourcalderon@gmail.com</p>
+          </li>
+          <li className='flex justify-between space-x-6'>
+            <p>GitHub:</p> <p>@rscalderon</p>
+          </li>
+          <li className='flex justify-between space-x-6'>
+            <p>LinkedIn:</p> <p>rodrigosamourcalderon</p>
+          </li>
+        </ol>
+        <button className='bg-slate-700 p-4 rounded-2xl self-center'>
+          <p className=''>Download contact (coming soon!) </p>
+        </button>
+        <Link
+          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
+          href='/'
+        >
+          Go to main page
+        </Link>
+      </main>
+    </div>
+  );
+}
