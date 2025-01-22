6b5c61023979bff9a52829e997e8f8afbd38a3f9
rscalderon <127636148+rscalderon@users.noreply.github.com>
2025-01-22 08:25:28 -0500

    centered text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,8 @@ export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 py-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='space-y-4'>
-        <h1 className='text-xl'>Welcome! I am Rodrigo S. Calderon</h1>
-        <p>This is v0 of my personal site.</p>
-        <section>
+        <h1 className='text-xl'>Welcome! I am Rodrigo S. Calderon.</h1>
+        <section className='flex space-x-1 justify-center w-full'>
           <Link
             href='https://www.linkedin.com/in/rodrigosamourcalderon/'
             target='_blank'
@@ -14,7 +13,7 @@ export default function Home() {
           >
             LinkedIn
           </Link>
-          {' | '}
+          <p>{'|'}</p>
           <Link
             href='https://www.github.com/rscalderon/'
             target='_blank'
@@ -22,7 +21,7 @@ export default function Home() {
           >
             GitHub
           </Link>
-          {' | '}
+          <p>{'|'}</p>
           <Link
             href={`mailto:carry_overage.8k@icloud.com?subject=Resume Request | Looking for <Enter position>&body=Hi Rodrigo, I hope you're doing well! %0D%0A %0D%0A Please share your most updated resume. I'm looking for <role you're looking for>. %0D%0A %0D%0A All the best, %0D%0A %0D%0A <Your name here>`}
             target='_blank'
@@ -30,7 +29,7 @@ export default function Home() {
           >
             Resume
           </Link>
-          {' | '}
+          <p>{'|'}</p>
           <Link
             href='https://medium.com/@samourcalderon'
             target='_blank'
