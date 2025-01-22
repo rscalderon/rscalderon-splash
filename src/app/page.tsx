6d6138388b59eb804f45d5b29d0b2e2456360c58
rscalderon <127636148+rscalderon@users.noreply.github.com>
2025-01-22 08:11:52 -0500

    nits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-// import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
@@ -7,7 +6,7 @@ export default function Home() {
       <main className='space-y-4'>
         <h1 className='text-xl'>Welcome! I am Rodrigo S. Calderon</h1>
         <p>
-          This is v0 of my personal site. An exciting update is coming soon!
+          This is v0 of my personal site. An exciting update is coming soon...
         </p>
         <section>
           <Link
@@ -88,54 +87,17 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      </main>*/}
+      <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
+          href='https://nextjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          Built with Next.js
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
+      </footer>
     </div>
   );
 }
