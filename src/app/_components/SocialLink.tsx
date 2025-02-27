8312b15d3bd87808ec4375afa6dc705023229e22
rscalderon <127636148+rscalderon@users.noreply.github.com>
2025-02-27 16:41:21 -0500

    reusable SocialLink component

---
+++ src/app/_components/SocialLink.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { JSX } from 'react';
+
+type SocialLinkProps = {
+  href: string;
+  className?: string;
+  children: string | JSX.Element;
+};
+
+export default function SocialLink({
+  href,
+  className = 'hover:underline hover:underline-offset-4',
+  children,
+}: SocialLinkProps) {
+  return (
+    <Link href={href} target='_blank' className={className}>
+      {children}
+    </Link>
+  );
+}
