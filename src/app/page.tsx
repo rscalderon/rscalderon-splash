e44061841ff18ffa7335e61a8022e79f6550a2b7
rscalderon <127636148+rscalderon@users.noreply.github.com>
2025-10-03 12:23:30 -0400

    Replace package-lock.json with pnpm-lock.yaml, add new logos, and implement marquee component for displaying work and study logos.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,53 @@
-import Link from 'next/link';
+import SocialLinksSection from './_components/SocialLinksSection';
+import Marquee from './_components/Marquee';
 
 export default function Home() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 py-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='space-y-4'>
-        <h1 className='text-xl'>Welcome! I&apos;m Rodrigo S. Calderon.</h1>
-        <section className='flex space-x-1 justify-center w-full'>
-          <Link
-            href='https://www.linkedin.com/in/rodrigosamourcalderon/'
-            target='_blank'
-            className='hover:underline hover:underline-offset-4'
-          >
-            LinkedIn
-          </Link>
-          <p>{'|'}</p>
-          <Link
-            href='https://www.github.com/rscalderon/'
-            target='_blank'
-            className='hover:underline hover:underline-offset-4'
-          >
-            GitHub
-          </Link>
-          <p>{'|'}</p>
-          <Link
-            href={`mailto:carry_overage.8k@icloud.com?subject=Resume Request | Looking for <Enter position>&body=Hi Rodrigo, I hope you're doing well! %0D%0A %0D%0A Please share your most updated resume. I'm looking for <role you're looking for>. %0D%0A %0D%0A All the best, %0D%0A %0D%0A <Your name here>`}
-            target='_blank'
-            className='hover:underline hover:underline-offset-4'
-          >
-            Resume
-          </Link>
-          <p>{'|'}</p>
-          <Link
-            href='https://medium.com/@samourcalderon'
-            target='_blank'
-            className='hover:underline hover:underline-offset-4'
-          >
-            Medium
-          </Link>
-        </section>
-      </main>
-      {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <main className='grid grid-rows-[100px_1fr_100px] items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <section className='space-y-2'>
+        <h1 className='text-xl text-center'>
+          Welcome! I&apos;m Rodrigo S. Calderon.
+        </h1>
+        <SocialLinksSection />
+        <p className='text-center animate-pulse'>
+          This is an active construction (web)site 🚧
+        </p>
+      </section>
+      <section>
+        <Marquee variant='work' />
+        <Marquee variant='study' />
+      </section>
+      <footer className='flex gap-6 flex-wrap items-center justify-center'>
+        <p className='flex gap-1'>
+          {/* Built with
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='flex items-center hover:underline hover:underline-offset-4'
+            href='https://nextjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Next.js
           </a>
+          | */}
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='flex items-center hover:underline hover:underline-offset-4'
+            href='https://github.com/rscalderon/rscalderon-splash'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            Read our docs
+            Check out my code
           </a>
-        </div>
-      </main>*/}
-      <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
-        <a
-          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-          href='https://nextjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Built with Next.js
-        </a>
+          or
+          <a
+            className='flex items-center hover:underline hover:underline-offset-4'
+            href='https://monte-carlo-simulator-seven.vercel.app/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            some toy projects
+          </a>
+        </p>
       </footer>
-    </div>
+    </main>
   );
 }
